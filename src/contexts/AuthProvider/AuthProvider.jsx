1a6e9726b3419af021d7c36ebf63340717619ace
Joy <kaosarjoy52@gmail.com>
2026-05-11 22:48:43 +0600

    set up firebase env

---
+++ src/contexts/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,27 @@
+import {  signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { AuthContext } from '../AuthContext/AuthContext';
+import { auth } from '../../firebase/firebase.init';
+
+const AuthProvider = ({children}) => {
+   
+    const registerUser = (email,password)=>{
+     return  createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signInUser =( email, password)=>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    
+    const authInfo = {
+        registerUser,
+        signInUser
+    }
+    return (
+        <AuthContext value={authInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,11 +1,19 @@
 import { useForm } from "react-hook-form";
+import UseAuth from "../../../hooks/UseAuth";
 
 
 const Register = () => {
     const {register,  handleSubmit, formState: { errors },} = useForm();
-
+    const {registerUser}=UseAuth();
     const handleRegister = (data)=>{
-        console.log("use data", data);
+       // console.log("use data", data);
+       registerUser(data.email, data.password)
+       .then((result)=>{
+        const user = result.user;
+        console.log("registered user", user);
+       }).catch((error)=>{
+        console.log("registration error", error);
+       })
     }
 
     return (
@@ -16,12 +24,15 @@ const Register = () => {
           <input type="email" {...register("email",{required:true})} className="input" placeholder="Email" />
           {errors.email?.type==="required" && <p className="text-red-500">Email is required</p>}
           {errors.email?.type==="pattern" && <p className="text-red-500">Please enter a valid email</p>}
+
+            <label className="label">Image file</label>
+        <input type="file" className="file-input file-input-info" />
           <label className="label">Password</label>
           <input type="password" {...register("password", {required:true,pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/})} className="input" placeholder="Password" />
           {errors.password?.type==="required" && <p className="text-red-500">Password is required</p>}
           {errors.password?.type==="pattern" && <p className="text-red-500">Password must be at least 6 characters long and include an uppercase letter, a lowercase letter, a number, and a special character</p>}
           <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Login</button>
+          <button className="btn btn-neutral mt-4">Register</button>
         </fieldset>
             </form>
         </div>
